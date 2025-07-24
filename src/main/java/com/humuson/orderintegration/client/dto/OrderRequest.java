e484222394e80187dea473b6dcc02796c9686b13
Hyejeong <thswjd0609@gmail.com>
2025-07-24 17:14:49 +0900

    feat: dto 작성 (#5)

---
+++ src/main/java/com/humuson/orderintegration/client/dto/OrderRequest.java
@@ -0,0 +1,18 @@
+package com.humuson.orderintegration.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderRequest {
+    private String orderId;
+    private String customerName;
+    private String orderDate;
+    private String status;
+    private String description;
+}
